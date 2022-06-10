568db3feed849cb63cf10e22c793a792d5970b57
Zack Rogers <zack.rogers.t@gmail.com>
2022-06-10 10:27:42 -0600

    added part 4 js

---
+++ profile.js
@@ -0,0 +1,20 @@
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+let colorBtn = document.getElementById("color")
+let placeBtn = document.getElementById("place")
+let ritualBtn = document.getElementById("ritual")
+
+
+colorBtn.addEventListener('click', () => {
+    alert('My favorite color is green');
+});
+
+ritualBtn.addEventListener('click', () => {
+    alert('My favorite ritual is going to the gym');
+});
+
+placeBtn.addEventListener('click', () => {
+    alert('My favorite place is the gym');
+});
